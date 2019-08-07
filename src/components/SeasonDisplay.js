a51fa9e47e14acf421df9fa6153f6cd03d82e89e
Sean <writeseanhong@gmail.com>
2019-08-07 10:41:43 -0500

    added season config to refactor the conditional later

--- src/components/SeasonDisplay.js
+++ src/components/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from "react";
 
+const seasonConfig = {
+    summer: {
+        text: 'Lets hit the beach!',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Brr, it is cold!',
+        iconName: 'snowflake'
+    }
+}
+
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
         return lat > 0 ? "summer" : "winter";
@@ -12,12 +23,13 @@ const SeasonDisplay = (props) => {
 
     const season = getSeason(props.lat, new Date().getMonth());
     const text = season === "winter" ? "Brrr, it's chilly" : "Lets hit the beach!";
+    const icon = season === "winter" ? "snowflake" : "sun";
 
     return (
         <div>
-
+            <i className={`${icon} icon`}></i>
             <h1>{text}</h1>
-
+            <i className={`${icon} icon`}></i>
         </div>
 
     )
